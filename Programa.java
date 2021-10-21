64db6566315d41eb6a05c215704a9aa18a547580
LuanAngeloRocha <Luanrochaangelo@gmail.com>
2021-10-21 19:54:42 +0000

    b

---
+++ Programa.java
@@ -0,0 +1,21 @@
+public class Programa{
+    
+    public static void main (String args[]){
+        int numero = 10;
+        Atributo2  at = new Atributo2()
+
+  
+  while(true){
+    int cont = 0;
+    cont = cont + 1 ;
+    if (cont>0){
+      break;
+    }
+    
+  System.out.println(cont);
+
+  }
+  }
+
+
+}
